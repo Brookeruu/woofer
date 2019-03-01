0e35f48a57ecba2b937f206ce75d44a6ccbdf32a
Brooke <perrysbrooke@gmail.com>
2019-03-01 15:26:00 -0800

    add Pet properties, display on PetList

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import jupiter from './../images/jupiter.jpg';
+import jupiter from './../images/jupiter.png';
 
 
-export default function Pet() {
+export default function Pet(props) {
   
   return (
     <div>
-    <img  src={jupiter}
+
+      <img  
+          src={jupiter}
           style={{
-            width: '70px',
+            width: '200px',
             borderRadius: '50%'
           }}
-    >
-    </img>
-      <h3>Pet info add rest of related pet info to go in here</h3>
+      ></img>
+      <h3>{props.name}</h3>
+      <p>Microchip: {props.microchip}</p>
+      <p>Age: {props.age} yrs</p>
       
     </div>
   )
 }
+
+Pet.propTypes = {
+  name: PropTypes.string.isRequired,
+  microchip: PropTypes.string,
+  age: PropTypes.string,
+  image: PropTypes.string
+}
+
+
 
--- src/components/PetList.jsx
+++ src/components/PetList.jsx
@@ -3,13 +3,37 @@ import NewPet from './NewPet.jsx';
 import Pet from './Pet.jsx';
 import PropTypes from 'prop-types';
 
+const petList = [
+  {
+    name: 'Jupiter',
+    microchip: '1234567',
+    age: '2',
+    image: './../images/jupiter.png'
+  }
+  // {
+  //   name: 'Phantom',
+  //   microchip: '1234567',
+  //   age: '2',
+  //   image: './../images/phantom.png'
+  // }
+]
+
+
 export default function Petlist () {
   
   return (
     <div>
       <h3>This will be the List of Pets</h3>
       <NewPet />
-      <Pet />
+      {petList.map((pet, index) =>
+        <Pet 
+            name={pet.name}
+            microchip={pet.microchip}
+            age={pet.age}
+            image={pet.picture}
+            />
+      
+      )}
     </div>
   )
 }
